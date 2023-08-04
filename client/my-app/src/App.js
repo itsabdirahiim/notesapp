df10b11e30ec8777a7b7176a5f50028a9bcbbcd5
abdirahiim ahmed <itsabdirahiim@gmail.com>
2023-08-04 16:45:05 +0200

    #

--- client/my-app/src/App.js
+++ client/my-app/src/App.js
@@ -3,18 +3,20 @@ import Signup from "./com/signup";
 import Home from "./com/home";
 import Login from "./com/login";
 // import { Hompr } from "./com/homepr";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes , Link} from "react-router-dom";
 
 function App() {
   return (
     <div>
       <BrowserRouter>
         <Routes>
-          <Route path="login" element={<Login />} />
+          <Route path="/login" element={<Login />} />
           <Route path="/" element={<Home />} />
-          <Route path="signup" element={<Signup />} />
+          <Route path="/signup" element={<Signup />} />
         </Routes>
       </BrowserRouter>
+      <Link to="/">Home</Link>
+<Link to="/signup">Signup</Link>
     </div>
   );
 }
