a27db7561f563e8b293b7b6ab21386a9e8b7bc37
abdirahiim ahmed <itsabdirahiim@gmail.com>
2023-08-04 18:22:56 +0200

    ¤

--- client/my-app/src/App.js
+++ client/my-app/src/App.js
@@ -3,18 +3,18 @@ import Signup from "./com/signup";
 import Home from "./com/home";
 import Login from "./com/login";
 // import { Hompr } from "./com/homepr";
-import { BrowserRouter, Route, Routes} from "react-router-dom";
+import { HashRouter as Router, Route, Routes} from "react-router-dom";
 
 function App() {
   return (
     <div>
-      <BrowserRouter>
+      <Router>
         <Routes>
           <Route path="/login" element={<Login />} />
           <Route path="/" element={<Home />} />
           <Route path="/signup" element={<Signup />} />
         </Routes>
-      </BrowserRouter>
+      </Router>
     </div>
   );
 }
