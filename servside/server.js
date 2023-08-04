341cd289c22b2961ada582cb4b3761fad08c46d7
abdirahiim ahmed <itsabdirahiim@gmail.com>
2023-08-04 15:00:35 +0200

    #

--- servside/server.js
+++ servside/server.js
@@ -67,7 +67,7 @@ app.use("/", homer);
 app.use("/api", apir);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/my-app/build")));
-  app.get("*", function (req, res) {
+  app.get("/*", function (req, res) {
     res.sendFile(path.resolve(__dirname, "../client/my-app/build", "index.html"));
   });
 }
