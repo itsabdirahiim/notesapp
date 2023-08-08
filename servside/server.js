b70b7dbed2e2044b404b5648bac8dc65b4321db2
abdirahiim ahmed <itsabdirahiim@gmail.com>
2023-08-08 17:42:28 +0200

    #

--- servside/server.js
+++ servside/server.js
@@ -58,10 +58,7 @@ app.use(
 
 app.use(passport.initialize());
 app.use(passport.session());
-app.use((req,res,next)=>{
-  console.log(req.user)
-  next()
-})
+
 // app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static(path.join(__dirname, '../client/my-app/build')));
 app.get('*', function (req, res) {
