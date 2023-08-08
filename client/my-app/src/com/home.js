a0a933eb1f1c80989111dbcf79e238983904cb86
abdirahiim ahmed <itsabdirahiim@gmail.com>
2023-08-08 15:24:17 +0200

    #

--- client/my-app/src/com/home.js
+++ client/my-app/src/com/home.js
@@ -15,8 +15,8 @@ export default function Home() {
           return response.json();
         } else {
           // window.location.href = "/login";
-          // throw new Error("Unauthorized");
-          return <Login/>
+          throw new Error("Unauthorized");
+       
          
         }
       })
