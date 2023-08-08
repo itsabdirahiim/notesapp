a77d8255f5a59bdc573aa443f2421482c4805b0a
abdirahiim ahmed <itsabdirahiim@gmail.com>
2023-08-08 11:27:53 +0200

    #

--- servside/server.js
+++ servside/server.js
@@ -59,20 +59,19 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 // app.use(bodyParser.urlencoded({ extended: true }))
-app.use(express.static(path.join(__dirname, '../client/my-app/build')));
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/my-app/build")));
-  app.get("*", function (req, res) {
-    res.sendFile(path.join(__dirname + "../client/my-app/build/index.html" ));
-  });
-}
+app.use(express.static('../client/my-app/build'));
 // app.use(bodyParser.json());
 
 app.use(flash());
 
 app.use("/", homer);
 app.use("/api", apir);
-
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/my-app/build")));
+  app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname + "../client/my-app/build/index.html" ));
+  });
+}
 
 app.listen(PORT, () => {
   console.log(`its running ${PORT}`);
