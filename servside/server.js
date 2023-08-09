ec7b7806a48294513ebed2c434034ec571ddba32
abdirahiim ahmed <itsabdirahiim@gmail.com>
2023-08-09 10:13:23 +0200

    #

--- servside/server.js
+++ servside/server.js
@@ -66,6 +66,7 @@ app.use(passport.session());
 app.use(express.static(path.join(__dirname, '../client/my-app/build')));
 app.get('*', function (req, res) {
   res.sendFile(path.join(__dirname , '../client/my-app/build/index.html' ));
+  console.log(process.env.NODE_ENV)
   
 })
 
