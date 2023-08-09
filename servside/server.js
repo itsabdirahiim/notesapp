0a30fb14371d606d9fb518a02b09b98b21d92a87
abdirahiim ahmed <itsabdirahiim@gmail.com>
2023-08-09 18:06:09 +0200

    Initial commit

--- servside/server.js
+++ servside/server.js
@@ -60,10 +60,11 @@ app.use(cors(corsOptions));
 app.use(express.static(path.join(__dirname, '../client/my-app/build')));
 
 // Use API routes
-const homer = require("./routes/home");
-app.use("/", homer);
 const apir = require("./routes/api");
 app.use("/api", apir);
+const homer = require("./routes/home");
+app.use("/", homer);
+
 
 // Use main route handler
 app.get('*', function (req, res) {
